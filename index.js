dd7ed9dc82226539a1ff7637cbb6305e03e0d973
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-07 20:01:06 +0300

    commit tree e5dbd5143581beb0b58690f03172151968edb0f9

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+// variable called multiply, result of multiplication should be 62
+
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1*num2;
+
+//variable called random that will generate a random integer greater than 0
+let random = Math.floor(Math.random()*10)+1;
+
+//variable called mod set an equation will calculate remainder of dividing variable num3 by num4; remainder should be 4
+
+const num3 =29;
+const num4 =25;
+
+let mod = num3 % num4;
+
+//variable called max finds the highest number in a set
+let max = Math.max(1,20,3,14,9,10);
